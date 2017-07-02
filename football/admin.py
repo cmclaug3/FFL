2592ae42772333d5171ca761552edbe014c86865
Corey McLaughlin <mclaughlin848@gmail.com>
2017-07-02 15:56:02 -0600

    redid models, added notes

--- football/admin.py
+++ football/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from ffl.models import Stat
+
+
+admin.site.register(Stat)
+
+
+
--- football/models.py
+++ football/models.py
@@ -6,11 +6,18 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class Stat(models.Model):
+	team = models.ForiegnKey(User)
+	game = models.IntegerField()
+	points_for = FloatField()
+	points_against = FloatField()
 
-class Owner(models.Model):
-	owner = models.ForeignKey(User)
-	date = models.DateField()
+	def __str__(self):
+		pass
+
+	def record(self):
+		pass
 
 
-class Stats(models.Model):
-	
+
+
